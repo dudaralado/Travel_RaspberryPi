08b9a4d798c915773746d3d3eae2db0e000c3274
Duda Ralado <fdudus@gmail.com>
2025-03-31 18:57:08 +0100

    Update tmux.conf

--- packages.sh
+++ packages.sh
@@ -9,9 +9,13 @@ sudo apt upgrade -y
 
 ## Installing needed software
 
-sudo apt install -y  isc-dhcp-server bind9 bind9utils bind9-doc dnsutils firewalld vim haproxy x11-xkb-utils
+sudo apt install -y  isc-dhcp-server bind9 bind9utils bind9-doc dnsutils firewalld vim haproxy golang
 
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo  tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update && sudo apt install -y eza
+
+go install github.com/jesseduffield/lazygit@latest
+
+
